00160a88505cc6b6675c4acdebd4293437f2933b
Surojit Saha <sahasurojit06@gmail.com>
2022-05-21 17:15:32 +0530

    Added requirements.txt and README.md

---
+++ README.md
@@ -0,0 +1,47 @@
+
+# Mobile Price Classifier
+
+Developed a machine learning model to predict the price range of mobile phone.
+
+## Features
+
+- Used technologies like Google Collaboratory and Python packages like Numpy, Pandas, Matplotlib, Seaborn and Scikit-learn
+- Data Visualization for better understanding
+- Cross Platform
+- Improved accuracy of the model up to 93% using complex algorithms
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/surojitsaha56/Mobile_Price_Classifier.git
+```
+
+
+Install dependencies
+
+```bash
+  pip install -r requirements.txt
+```
+
+Go to the project directory
+
+```bash
+  cd Mobile_Price_Classifier
+```
+
+Start the server
+
+```bash
+  flask run
+```
+
+
+## Tech Stack
+
+**Client:** HTML, CSS
+
+**Server:** Flask
+
--- app.py
+++ app.py
@@ -3,22 +3,12 @@ import flask
 import pickle
 import pandas as pd
 import numpy as np
-
 import random
-
-
-
-
 from sklearn.preprocessing import StandardScaler
-
 
 #load models at top of app to load into memory only one time
 with open('models/Dtree.pickle', 'rb') as f:
     clf_dtree= pickle.load(f)
-
-
-
-
 
 app = flask.Flask(__name__, template_folder='templates')
 @app.route('/result',methods=['GET','POST'])
@@ -30,8 +20,6 @@ def result():
 @app.route('/', methods = ['GET', 'POST'])
 def main():
         return flask.render_template('home.html')
-
-
 
 @app.route("/mobile", methods=['GET', 'POST'])
 def mobile():
@@ -80,27 +68,24 @@ def mobile():
 
         pred = clf_dtree.predict([x])
 
-        price = 0
-        #Predicting price
-        if pred == [0]:
-            price = random.randrange(5000, 7000, 500)
-        elif pred == [1]:
-            price = random.randrange(7000, 10000, 500)
-        elif pred == [2]:
-            price = random.randrange(10000, 15000, 500)
-        else:
-            price = random.randrange(15000, 30000, 500)
+        # price = 0
+        # #Predicting price
+        # if pred == [0]:
+        #     price = random.randrange(5000, 7000, 500)
+        # elif pred == [1]:
+        #     price = random.randrange(7000, 10000, 500)
+        # elif pred == [2]:
+        #     price = random.randrange(10000, 15000, 500)
+        # else:
+        #     price = random.randrange(15000, 30000, 500)
 
 
         print("Congrats",clf_dtree.predict([x]))
         
         return flask.render_template('result.html', 
                                      original_input=output_dict,
-                                     result = pred, price = price)
+                                     result = pred)
 
 
-        
-        
-      
 if __name__ == '__main__':
     app.run(debug=True)
